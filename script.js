8bd68621113b58887750a55aef31bf6740409869
Hojoon Kim <hojoon0724@gmail.com>
2023-12-02 09:50:01 -0800

    Able to retrieve data from API and access the info with dot

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("bussin");
+// -----------------------------------------------------
+// Base elements
+// -----------------------------------------------------
+const baseURL = "https://api.spaceflightnewsapi.net/v4/articles/";
+const fileFormatRequested = "format=json";
+let limitOfArticlesAmount = 12;
+const limitOfArticles = `limit=${limitOfArticlesAmount}`;
+let newsArray = [];
+
+// URL assembly
+const url = `${baseURL}?${limitOfArticles}&${fileFormatRequested}`;
+
+// Fetch the stuff
+function getNews() {
+  fetch(url)
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(`SHOW "DATA.RESULTS" ${data.results}`);
+      newsArray = data.results;
+      //   console.log(`print array ${newsArray[0].title}`);
+      renderNews();
+    });
+}
+
+// Rendering to the screen
+function renderNews() {
+  console.log(`title: ${newsArray[0].title}`);
+  console.log(`url: ${newsArray[0].url}`);
+  console.log(`news_site: ${newsArray[0].news_site}`);
+  console.log(`summary: ${newsArray[0].summary}`);
+}
+// -----------------------------------------------------
+// Running Code
+// -----------------------------------------------------
+
+getNews();
