841caf8577a1fa5e2d4297eb5694ca7d79808b66
Hojoon Kim <hojoon0724@gmail.com>
2023-12-05 20:03:49 -0800

    cleaned up JS, improved CSS, working to get continuous browser width value

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// -----------------------------------------------------
 // Base elements
-// -----------------------------------------------------
 const baseURL = "https://api.spaceflightnewsapi.net/v4/articles/";
 const fileFormatRequested = "format=json";
 const $topContainer = $(".top-container");
@@ -10,14 +8,9 @@ let newsArray = [];
 
 // URL assembly
 const url = `${baseURL}?${limitOfArticles}&${fileFormatRequested}`;
-// const urlManual = `${baseURL}?search=spacex&${limitOfArticles}&${fileFormatRequested}`
 
 // Fetch the stuff
-
 function getNews() {
-  //! Change it back to "url" when it's for realz
-  // "/api-response-placeholder.json"
-  // fetch("/api-response-placeholder.json");
   fetch(url)
     .then((res) => {
       return res.json();
@@ -27,30 +20,20 @@ function getNews() {
       for (i = 0; i < limitOfArticlesAmount; i++) {
         renderNews();
       }
-
-      console.log(data.results);
-      console.log(url);
     });
 }
 
 function searchNews(input) {
-  //! Change it back to "url" when it's for realz
-  // "/api-response-placeholder.json"
-  // fetch("/api-response-placeholder.json")
   const searchURL = `${baseURL}?search=${input}&${limitOfArticles}&${fileFormatRequested}`;
   fetch(searchURL)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      console.log(data.results);
       newsArray = data.results;
       for (i = 0; i < limitOfArticlesAmount; i++) {
         renderNews();
       }
-
-      console.log(data.results);
-      console.log(searchURL);
     });
 }
 
@@ -60,14 +43,10 @@ function searchEvent(event) {
   const form = event.target;
   const formData = new FormData(form);
   const userSearch = formData.get("user-input");
-  console.log(userSearch);
   searchNews(userSearch);
-  // renderNews();
 }
 
-// Rendering to the screen
 function renderNews() {
-  // for (i = 0; i < limitOfArticlesAmount; i++) {
   // Make tile
   let $newsBoxContainer = $("<div>").attr("class", "news-box-container");
   $topContainer.append($newsBoxContainer);
@@ -77,20 +56,15 @@ function renderNews() {
   $newsPhotoContainer.html(`<img src="${newsArray[i].image_url}">`);
   $newsBoxContainer.append($newsPhotoContainer);
 
-  // Text Container
+  // Text Containers
   let $newsTextContainer = $("<div>").attr("class", "news-text-container");
   $newsBoxContainer.append($newsTextContainer);
-
   let $newsDetailsAbsoluteContainer = $("<div>").attr("class", "news-details-absolute-container");
   $newsBoxContainer.append($newsDetailsAbsoluteContainer);
   let $newsDetailsContainer = $("<div>").attr("class", "news-details-container");
   $newsDetailsAbsoluteContainer.append($newsDetailsContainer);
-
-  // Photo overlays
-  let $photoOverlay = $("<div>").attr("class", "photo-overlay");
-  $newsBoxContainer.append($photoOverlay);
-  let $fullOverlay = $("<div>").attr("class", "full-overlay");
-  $newsBoxContainer.append($fullOverlay);
+  let $newsSummaryContainer = $("<div>").attr("class", "news-summary-container");
+  $newsDetailsContainer.append($newsSummaryContainer);
 
   // Text Top+Bottom Sections
   let $newsTextTop = $("<div>").attr("class", "news-text-top-container");
@@ -98,52 +72,25 @@ function renderNews() {
   let $newsTextBottom = $("<div>").attr("class", "news-text-bottom-container");
   $newsTextContainer.append($newsTextBottom);
 
-  // Actual Content
+  // Photo overlays
+  let $photoOverlay = $("<div>").attr("class", "photo-overlay");
+  $newsBoxContainer.append($photoOverlay);
+  let $fullOverlay = $("<div>").attr("class", "full-overlay");
+  $newsBoxContainer.append($fullOverlay);
+
+  // Text
   let $newsTitle = $("<div>").attr("class", "news-title").text(`${newsArray[i].title}`);
   $newsTextBottom.append($newsTitle);
-
-  let $newsSummaryContainer = $("<div>").attr("class", "news-summary-container");
-  $newsDetailsContainer.append($newsSummaryContainer);
   let $newsSummary = $("<div>").attr("class", "news-summary").text(`${newsArray[i].summary}`);
   $newsSummaryContainer.append($newsSummary);
-
-  // let $newsSource = $("<div>").attr("class", "news-source-link").html(`<a href="${newsArray[i].url}"><button>Read More at ${newsArray[i].news_site}</button></a>`).css("margin-top", "16px");
   let $newsSource = $("<div>").attr("class", "news-source-link").html(`<a href="${newsArray[i].url}"><button>Full Article</button></a>`);
   $newsDetailsContainer.append($newsSource);
-
   let $newsSite = $("<div>").attr("class", "news-site").text(`${newsArray[i].news_site}`);
   $newsTextTop.append($newsSite);
-
   let $newsTime = $("<div>").attr("class", "news-time").text(`${newsArray[i].published_at}`);
   $newsTextTop.append($newsTime);
 
-  //* Click event listener
-  /*
-  let open = false;
-  $newsBoxContainer.on("click", (event) => {
-    open = !open;
-    function openNews() {
-      $newsTitle.css("opacity", 0);
-      $newsSummary.css("opacity", 1);
-      $fullOverlay.css("opacity", 1);
-      $newsTextTop.css("opacity", 0);
-    }
-    function closeNews() {
-      $newsTitle.css("opacity", 1);
-      $newsSummary.css("opacity", 0);
-      $fullOverlay.css("opacity", 0);
-      $newsTextTop.css("opacity", 1);
-    }
-    if (open) {
-      openNews();
-      console.log(`openNews ${open}`);
-    } else {
-      closeNews();
-      console.log(`closeNews ${open}`);
-    }
-  });
-*/
-  //* Hover event listener
+  // Hover event listener
   $newsBoxContainer.on("mouseenter", (event) => {
     $newsTitle.css("opacity", 0);
     $newsDetailsAbsoluteContainer.css("opacity", 1);
@@ -157,53 +104,23 @@ function renderNews() {
     $fullOverlay.css("opacity", 0);
     $newsTextTop.css("opacity", 1);
   });
-
-  //   "click", (event) => {
-  //   $newsTextBottom.css("border", "0px");
-  //   $newsTitle.css("opacity", 0);
-  //   $newsSummary.css("display", "flex");
-  // });
 }
 
-// -----------------------------------------------------
-// Running Code
-// -----------------------------------------------------
-
-// Default Load
+// Default Load the Latest
 getNews();
 
-// -----------------------------------------------------
-// User Interaction
-// -----------------------------------------------------
-
-//! Article click hides title, adds an overlay, shows summary + link
-// function sayClick() {
-//   $newsBoxContainer.css("border", "none");
-// }
-
-// $(document).on("click", ".news-box-container", sayClick);
-
-// Testing Area-----------------------
-
-// Testing Area-----------------------
-
-// -----------------------------------------------------
-// CSS DOM Manipulation
-// -----------------------------------------------------
-
-// Fade out Title when searching
+// Fade out Title when searching in mobile
 let $mobileWidth = $(window).width();
 const $titleContainer = $(".title-container");
 const $search = $(".search");
-
 $search.on("click", fadeTitleInMobileWhenSearching());
 
 function fadeTitleInMobileWhenSearching() {
-  window.onresize = $mobileWidth;
-  if ($mobileWidth < 786) {
+  if ($mobileWidth < 767) {
     $search.on("focus", (event) => $titleContainer.css("opacity", 0));
     $search.on("blur", (event) => $titleContainer.css("opacity", 1));
   }
 }
 
+// Search event listener
 document.querySelector("form").addEventListener("submit", searchEvent);
